e90d6436afa95abc61cbf3e8a2e35289f4fcd927
Pionpill <673486387@qq.com>
2021-12-15 13:37:49 +0800

    ...

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.backend.repository.AccountRepository;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+    @Autowired
+    private AccountRepository accountRepository;
+
 	@Test
-	void contextLoads() {
+    void findAll() {
+        System.out.println(accountRepository.findAll());
 	}
 
 }
