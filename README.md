d466d2a24025aff590e0b11717cbdd1b3fd053bd
Pionpill <673486387@qq.com>
2021-12-15 11:46:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+文件说明:
+- Collage-J2EE
+  - documents: 文档文件，文档用 .md 写，只允许放入文本文件
+  - interface: 前端主界面，使用 vue3 开发，vue 比较简单易学，基本上有手就行
+    - vue 安装教程(VScode): [CSDN](https://blog.csdn.net/weixin_42315600/article/details/100044533)
+  - backend: 后端，使用 springboot 框架，mysql 数据库
+  - sql: 数据库脚本
---
+++ interface/src/views/Warehouse.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <td>编号</td>
+        <td>图书名称</td>
+        <td>作者</td>
+      </tr>
+      <tr v-for="item in books">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.author }}</td>
+      </tr>
+    </table>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Books",
+  data() {
+    return {
+      msg: "Hello Vue",
+      books: [
+        {
+          id: 1,
+          name: "Java 零基础实战",
+          author: "Pionpill",
+        },
+        {
+          id: 2,
+          name: "Python 零基础实战",
+          author: "小鸡炖蘑菇",
+        },
+        {
+          id: 3,
+          name: "JavaScript 零基础实战",
+          author: "小葱拌豆腐",
+        },
+      ],
+    };
+  },
+};
+</script>
