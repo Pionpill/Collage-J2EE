d9bbf8b954dbe4e4e3824ed67d1965f8fb539c06
Pionpill <673486387@qq.com>
2021-12-18 13:26:05 +0800

    ...

---
+++ interface/src/components/library/LibraryIndex.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-container>
+    <el-aside style="width: 200px;margin-top: 20px">
+      <switch></switch>
+      <SideMenu></SideMenu>
+    </el-aside>
+    <el-main>
+      <!--<books></books>-->
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import SideMenu from "./SideMenu.vue";
+export default {
+  name: "AppLibrary",
+  components: { SideMenu }
+};
+</script>
+
+<style scoped></style>
